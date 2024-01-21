1f519d1b8aee155277aa22f50e3e034e7bdba1df
MaralTach <yoloten.maral@gmail.com>
2024-01-21 19:08:04 +0300

    Clrswy Paths website

---
+++ script.js
@@ -0,0 +1,20 @@
+// const x = 5;
+// const y = 10;
+ 
+// if ((x+y) % x && y<15){
+//     console.log("both condiotions are true");
+// } else {
+//     console.log("at least one condition is false");
+// }
+
+
+// if(21 %6 && 14 % 2 || !false){
+//     console.log("hello")
+// }else if("3" + 4 >33){
+//     console.log("Ch16")
+// } else{
+//     console.log("world")
+// }
+
+// const result = "65" === 65 ? "Success" : "Fail";
+// console.log(result)
